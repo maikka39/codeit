7929f9cfa3f0889adcaebd35aa7ce40ded222fe1
Maik de Kruif <maikka39@gmail.com>
2018-03-19 11:49:59 +0100

    Added some meta lags to the head

--- include/head.php
+++ include/head.php
@@ -4,12 +4,21 @@
   }
 </script> -->
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Default meta stuff -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+<!-- Define main stylesheets -->
 <link rel="stylesheet" href="/css/fonts.css">
 <link rel="stylesheet" href="/css/nav.css">
 <link rel="stylesheet" href="/css/master.css">
 
+<!-- Define default stuff -->
+<link rel="icon" type="image/png" sizes="192x192" href="img/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="img/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png">
+
+<!-- Define apple stuff -->
 <link rel="apple-touch-icon" sizes="57x57" href="img/icons/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="img/icons/apple-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="img/icons/apple-icon-72x72.png">
@@ -19,18 +28,36 @@
 <link rel="apple-touch-icon" sizes="144x144" href="img/icons/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="img/icons/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192" href="img/icons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="img/icons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png">
+
+<!-- Define android stuff -->
 <link rel="manifest" href="manifest.json">
+
+<!-- Difine Microsoft stuff -->
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="img/icons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
+<!-- Define property values -->
+<meta property="og:site_name" content="CodeIt" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:locale:alternate" content="en_GB" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="CodeIt - Start coding today!" />
+<meta property="og:description" content="Codeit is an education company. But not one in the way you might think. We're committed to building the best learning experience inside and out, making Codeit the best place for our team to learn, teach, and create the online learning experience of the future." />
+<meta property="og:image:url" content="http://maikka39.tk/img/logo+text.png" />
+<meta property="og:image:secure_url" content="https://maikka39.tk/img/logo+text.png" />
+
+<!-- Define values for twitter cards -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@codeit">
+<meta name="twitter:creator" content="@93maikka39">
+<meta name="twitter:title" content="CodeIt - Start coding today!">
+<meta name="twitter:description" content="Codeit is an education company. But not one in the way you might think. We're committed to building the best learning experience inside and out, making Codeit the best place for our team to learn, teach, and create the online learning experience of the future.">
+<meta name="twitter:image" content="/img/screenshot.png">
+
 <!-- Import jQuery -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
 <script src="/js/lib/jQuery/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery1.min.js"></script> -->
 
 <!-- Import the cookies library -->
 <script src="/js/lib/cookies/cookies.js"></script>
