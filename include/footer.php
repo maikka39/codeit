b856d6edbc103528146f62dca9364e3d9b3fda2a
Maik de Kruif <maikka39@gmail.com>
2018-03-20 10:04:43 +0100

    Fixed the bug with the footer

--- include/footer.php
+++ include/footer.php
@@ -1,39 +1,39 @@
 <footer>
   <div class="footer-menu">
-    <ul>
-      <li class="footer-menu-head"><a href="/">Codeit</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
-      <li><a href="/jobs">Jobs</a></li>
-    </ul>
-    <ul>
-      <li class="footer-menu-head"><a href="/catalog">Catalog</a></li>
-      <!-- <li class="footer-menu-subhead">By Subject</li> -->
-      <li><a href="/catalog/#web-development">Web Development</a></li>
-      <li><a href="/catalog/#programming">Programming</a></li>
-      <li><a href="/catalog/#data-science">Data Science</a></li>
-      <li></li>
-    </ul>
-    <ul>
-      <li class="footer-menu-head"><a href="/languages">Languages</a></li>
-      <!-- <li class="footer-menu-subhead">By language</li> -->
-      <li><a href="/languages/java">Java</a></li>
-      <li><a href="/languages/ruby">Ruby</a></li>
-      <li><a href="/languages/python">Python</a></li>
-      <li><a href="/languages/js">JavaScript</a></li>
-    </ul>
-    <ul>
-      <li class="footer-menu-head"><a href="/recources">Resources</a></li>
-      <li><a href="/recources/beta-courses">Beta Courses</a></li>
-      <li><a href="/recources/articles">Articles</a></li>
-      <li><a href="/recources/support">Support</a></li>
-    </ul>
-    <ul>
-      <li class="footer-menu-head"><a href="/account">My Account</a></li>
-      <li><a href="/login">Login</a></li>
-      <li><a href="/account/premium">Premium</a></li>
-      <li><a href="/account/help">Help</a></li>
-    </ul>
+    <div class="nav-list">
+      <div class="nav-list-item footer-menu-head"><a href="/">Codeit</a></div>
+      <div class="nav-list-item"><a href="/about">About</a></div>
+      <div class="nav-list-item"><a href="/contact">Contact</a></div>
+      <div class="nav-list-item"><a href="/jobs">Jobs</a></div>
+    </div>
+    <div class="nav-list">
+      <div class="nav-list-item footer-menu-head"><a href="/catalog">Catalog</a></div>
+      <!-- <div class="nav-list-item" class="footer-menu-subhead">By Subject</div> -->
+      <div class="nav-list-item"><a href="/catalog/#web-development">Web Development</a></div>
+      <div class="nav-list-item"><a href="/catalog/#programming">Programming</a></div>
+      <div class="nav-list-item"><a href="/catalog/#data-science">Data Science</a></div>
+      <div class="nav-list-item"></div>
+    </div>
+    <div class="nav-list">
+      <div class="nav-list-item footer-menu-head"><a href="/languages">Languages</a></div>
+      <!-- <div class="nav-list-item" class="footer-menu-subhead">By language</div> -->
+      <div class="nav-list-item"><a href="/languages/java">Java</a></div>
+      <div class="nav-list-item"><a href="/languages/ruby">Ruby</a></div>
+      <div class="nav-list-item"><a href="/languages/python">Python</a></div>
+      <div class="nav-list-item"><a href="/languages/js">JavaScript</a></div>
+    </div>
+    <div class="nav-list">
+      <div class="nav-list-item footer-menu-head"><a href="/recources">Resources</a></div>
+      <div class="nav-list-item"><a href="/recources/beta-courses">Beta Courses</a></div>
+      <div class="nav-list-item"><a href="/recources/articles">Articles</a></div>
+      <div class="nav-list-item"><a href="/recources/support">Support</a></div>
+    </div>
+    <div class="nav-list">
+      <div class="nav-list-item footer-menu-head"><a href="/account">My Account</a></div>
+      <div class="nav-list-item"><a href="/login">Login</a></div>
+      <div class="nav-list-item"><a href="/account/premium">Premium</a></div>
+      <div class="nav-list-item"><a href="/account/help">Help</a></div>
+    </div>
   </div>
   <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/easteregg.php'; ?>
   <div id="easteregg" class="easteregg"></div>
